482aaa7a2238f4ff30a48ceeec5c66339943bf3c
Son Dang <minhson0506@gmail.com>
2022-02-28 09:48:41 +0200

    add filter function

--- components/List.js
+++ components/List.js
@@ -13,7 +13,8 @@ const List = ({navigation, myFilesOnly = false}) => {
   // const {userArray} = useUser(token);
   // console.log('users', userArray);
   const {mediaArray} = useMedia(myFilesOnly);
-  const [media, setMedia] = useState(mediaArray);
+  const [media, setMedia] = useState([]);
+  // console.log('media begin', mediaArray);
   // const [mediaArray, setMediaArray] = useMedia(myFilesOnly);
   // const [media, setMedia] = useState([]);
   const myAdditionData = JSON.parse(user.full_name);
@@ -21,17 +22,28 @@ const List = ({navigation, myFilesOnly = false}) => {
   const numberDisplay = 5;
 
   const filterData = async () => {
+    setMedia(mediaArray);
     console.log('my file only', myFilesOnly);
+    // setMedia(mediaArray);
     // console.log('interested', myAdditionData.interested);
     if (!myFilesOnly) {
-      // console.log('media Data', mediaArray);
+      console.log('media Data original', mediaArray);
+
+      //filter avatar media
       let array = mediaArray.filter(
         (obj) => obj.title.toLowerCase() === 'avatar'
       );
+
+      console.log('media data after filter avatar', array);
+      //filter current user
       array = array.filter((obj) => obj.user_id !== user.user_id);
-      setMedia(array); //ok
-      console.log('array', array);
-      const userData = await Promise.all(
+      console.log(
+        'media array after filter avatar and user before state',
+        array
+      );
+
+      //switch to user
+      let userData = await Promise.all(
         array.map(async (obj) => {
           const userByFile = await getUserById(obj.user_id, token);
           const additionData = JSON.parse(userByFile.full_name);
@@ -39,167 +51,113 @@ const List = ({navigation, myFilesOnly = false}) => {
           return userByFile;
         })
       );
+
+      console.log('user data start', userData);
+
+      console.log('current user', myAdditionData);
+      // filter by gender
+      if (myAdditionData.gender !== 'nonbinary') {
+        userData = userData.filter((obj) => {
+          // console.log('gender', obj.full_name.gender);
+          return obj.full_name.gender === myAdditionData.interested;
+        });
+      }
+      console.log('length after filter gender', userData.length);
+
+      if (userData.length > 5) {
+        if (myAdditionData.preference_location.toLowerCase() !== 'none')
+          userData = userData.filter((obj) => {
+            return (
+              obj.full_name.location === myAdditionData.preference_location
+            );
+          });
+      }
+      console.log('length after filter location', userData.length);
+
+      if (userData.length > 5) {
+        if (myAdditionData.preference_drinking.toLowerCase() !== 'none')
+          userData = userData.filter((obj) => {
+            return obj.full_name.driking === myAdditionData.preference_drinking;
+          });
+      }
+      console.log('length after filter drinking', userData.length);
+
+      if (userData.length > 5) {
+        if (myAdditionData.preference_nationality.toLowerCase() !== 'none')
+          userData = userData.filter((obj) => {
+            return (
+              obj.full_name.nationality ===
+              myAdditionData.preference_nationality
+            );
+          });
+      }
+      console.log('length after filter nationality', userData.length);
+
+      if (userData.length > 5) {
+        if (myAdditionData.preference_pet.toLowerCase() !== 'none')
+          userData = userData.filter((obj) => {
+            return obj.full_name.pet === myAdditionData.preference_pet;
+          });
+      }
+      console.log('length after filter pet', userData.length);
+
+      if (userData.length > 5) {
+        if (myAdditionData.age_range.toLowerCase() !== 'none') {
+          const age = myAdditionData.age_range.split('-');
+          userData = userData.filter((obj) => {
+            return (
+              obj.full_name.age >= parseInt(age[0]) &&
+              obj.full_name.age <= parseInt(age[1])
+            );
+          });
+        }
+      }
+      console.log('length after filter age', userData.length);
+
+      if (userData.length > 5) {
+        if (myAdditionData.preference_height.toLowerCase() !== 'none') {
+          const height = myAdditionData.age_range.split('-');
+          userData = userData.filter((obj) => {
+            return (
+              obj.full_name.height >= parseInt(height[0]) &&
+              obj.full_name.height <= parseInt(height[1])
+            );
+          });
+        }
+      }
+      console.log('length after filter height', userData.length);
+
+      console.log('user data after filter', userData);
+
       // setMedia(array);
       // console.log('user array modified', userData);
 
-      // filter file by user
+      // switch user to file
+      userData = userData.map((obj) => {
+        return obj.user_id;
+      });
+
+      // console.log('user_id', userData);
+      // console.log('media array after filter user ', media);
+      array = array.filter((obj) => {
+        return userData.includes(obj.user_id);
+      });
 
       // //Random 5 person for display
-      const length = array.length;
-      const randomArray = [
-        Math.floor(Math.random() * length),
-        Math.floor(Math.random() * length),
-        Math.floor(Math.random() * length),
-        Math.floor(Math.random() * length),
-        Math.floor(Math.random() * length),
-      ];
-      for (let i = 0; i < numberDisplay, i++; ) {
-        for (let j = 0; j < i, j++; ) {
-          while (randomArray[j] === randomArray[i])
-            randomArray[i] = Math.floor(Math.random() * length);
-        }
-      }
-      console.log('random', randomArray);
+      // console.log('length', media.length);
+      // shuffle array and display random 5 users
+      array = array.sort(() => 0, 5 - Math.random());
+      if (array.length > 5) array = array.slice(0, 5);
 
-      if (array.length >= 5) {
-        const mediaData = [
-          array[randomArray[0]],
-          array[randomArray[1]],
-          array[randomArray[2]],
-          array[randomArray[3]],
-          array[randomArray[4]],
-        ];
-        console.log('mediaData', mediaData);
-        array = media.filter(
-          (obj) =>
-            obj === mediaData[0] ||
-            obj === mediaData[1] ||
-            obj === mediaData[2] ||
-            obj === mediaData[3] ||
-            obj === mediaData[4]
-        );
-        setMedia(array);
-        // if (media) setMedia(media);
-        // console.log('mediaData', media);
-      }
-      // setMedia(array);
-      console.log('mediaData after filter', media);
-      // array = array.filter(async (obj) => {
-      //   let additionData = {json: 'json'};
-      //   try {
-      //     const userByFile = await getUserById(obj.user_id, token);
-      //     additionData = JSON.parse(userByFile.full_name);
-      //     // if (myAdditionData.interested.toLowerCase() === 'male') {
-      //     //   console.log('gender of match', additionData.gender);
-      //     //   return additionData.gender === 'male';
-      //     // } else if (myAdditionData.interested.toLowerCase() === 'female') {
-      //     //   console.log('gender of match', additionData.gender);
-      //     //   return additionData.gender === 'female';
-      //     // }
-      //   } catch (err) {
-      //     console.error(err);
-      //   }
-      //   return obj.user_id === 527;
-      // });
-      // console.log('array after filter gender', array);
-      // setMedia(array);
-      // setMedia(
-      //   mediaArray.filter(async function (obj) {
-      //     try {
-      //       const userByFile = await getUserById(obj.user_id, token);
-      //       const additionData = JSON.parse(userByFile.full_name);
-      //       console.log('gender', additionData.gender);
-      //       if (myAdditionData.interested === 'male') {
-      //         // console.log('gender of match', additionData.gender);
-      //         return (
-      //           obj.title.toLowerCase() === 'avatar' &&
-      //           additionData.gender === 'male'
-      //         );
-      //       } else if (myAdditionData.interested === 'female') {
-      //         return (
-      //           obj.title.toLowerCase() === 'avatar' &&
-      //           additionData.gender === 'female'
-      //         );
-      //       } else return obj.title.toLowerCase() === 'avatar';
-      //     } catch (err) {
-      //       console.error(err);
-      //     }
-      //   })
-      // );
-      // await setMedia(
-      //   mediaArray.filter((obj) => obj.title.toLowerCase() === 'avatar')
-      // );
-      // //   setMedia(mediaArray);
-      // // console.log('data', mediaArray);
-      // //   // filter data not belong in current user
-      // //   mediaArray = media.filter((obj) => obj.user_id !== user.user_id);
-      // //   setMedia(mediaArray);
-      // //   // //TODO: match by preference
-      // //   // //filter by preference
-      // //   // //filter by sex
-      // await setMedia(
-      //   mediaArray.filter(async function (obj) {
-      //     try {
-      //       const userByFile = await getUserById(obj.user_id, token);
-      //       const additionData = JSON.parse(userByFile.full_name);
-      //       if (myAdditionData.interested.includes('male')) {
-      //         // console.log('gender of match', additionData.gender);
-      //         return additionData.gender === 'male';
-      //       } else if (myAdditionData.interested.includes('female')) {
-      //         return additionData.gender === 'female';
-      //       }
-      //     } catch (err) {
-      //       console.error(err);
-      //     }
-      //   })
-      // );
-      //   setMedia(mediaArray);
-      //   // // // console.log('media array after filter', data);
-      //   // // //Random 5 person for display
-      //   const length = mediaArray.length;
-      //   const randomArray = [
-      //     Math.floor(Math.random() * length),
-      //     Math.floor(Math.random() * length),
-      //     Math.floor(Math.random() * length),
-      //     Math.floor(Math.random() * length),
-      //     Math.floor(Math.random() * length),
-      //   ];
-      //   for (let i = 0; i < numberDisplay, i++; ) {
-      //     for (let j = 0; j < i, j++; ) {
-      //       while (randomArray[j] === randomArray[i])
-      //         randomArray[i] = Math.floor(Math.random() * length);
-      //     }
-      //   }
-      //   if (mediaArray.length >= 5) {
-      //     const mediaData = [
-      //       mediaArray[randomArray[0]],
-      //       mediaArray[randomArray[1]],
-      //       mediaArray[randomArray[2]],
-      //       mediaArray[randomArray[3]],
-      //       mediaArray[randomArray[4]],
-      //     ];
-      //     console.log('mediaData', mediaData);
-      //     mediaArray = media.filter(
-      //       (obj) =>
-      //         obj === mediaData[0] ||
-      //         obj === mediaData[1] ||
-      //         obj === mediaData[2] ||
-      //         obj === mediaData[3] ||
-      //         obj === mediaData[4]
-      //     );
-      //     setMedia(mediaArray);
-      //     // if (media) setMedia(media);
-      //     // console.log('mediaData', media);
-      //   }
-      //   setMedia(mediaArray);
+      // set data to display
+      setMedia(array);
+      console.log('media after filter', media);
     }
-    // setMedia(mediaArray);
-    // console.log('media array in ListItem', media);
   };
 
   useEffect(() => {
     filterData();
-  }, []);
+  }, [mediaArray]);
 
   return (
     <FlatList
