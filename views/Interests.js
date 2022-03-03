dbfb7f5238a28f42661e5312b2906629c374635b
Son Dang <minhson0506@gmail.com>
2022-03-03 20:43:02 +0200

    impelement interests function

--- views/Interests.js
+++ views/Interests.js
@@ -1,54 +1,126 @@
-import {SafeAreaView, Text, View, StyleSheet} from 'react-native';
-import React, {useState} from 'react';
-import {PropTypes} from 'prop-types';
+import {SafeAreaView, Text, View, StyleSheet, FlatList} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {array, PropTypes} from 'prop-types';
 import GlobalStyles from '../utils/GlobalStyles';
-import {CheckBox} from 'react-native-elements';
 import {Button} from 'react-native-paper';
+import {dataSource} from '../utils/data';
+import {MainContext} from '../contexts/MainContext';
+import {useUser} from '../hooks/ApiHooks';
 
 const Interests = ({navigation}) => {
-  const [yoga, setYoga] = useState(false);
-  const [pets, setPets] = useState(false);
-  const [food, setFood] = useState(false);
-  const [button, setButtonSelected] = useState(false);
+  const {putUser, getUserByToken} = useUser();
+  const {user, loading, setLoading, token} = useContext(MainContext);
+  let additionData = JSON.parse(user.full_name);
+  // let array = [];
+  const [media, setMedia] = useState([]);
+  const [arrayState, setArrayState] = useState(
+    additionData.interests.split(',')
+  );
+  let result = [];
 
-  // const array = [yoga, pets, food];
-  // let checkedInterests = '';
-  const updateInterest = (interest) => {
-    console.log('interests', interest);
+  const arrayLoading = async () => {
+    const user = await getUserByToken(token);
+    additionData = JSON.parse(user.full_name);
+    const arrayInteres = additionData.interests.split(',');
+    let number = 0;
+    // for (let i = 1; i < dataSource.length; i++) {
+    result = dataSource.map((value) => {
+      const obj = {
+        id: number++,
+        name: value,
+        selected: arrayInteres.includes(value) ? true : false,
+      };
+      return obj;
+    });
+    setArrayState(result);
+    setMedia(result);
+    // console.log('datasouce', result);
   };
+
+  const onPressHandler = async (id) => {
+    console.log('id', id);
+    const array = media;
+    // console.log('array', array);
+    // if (array[id].selected === true) {
+    //   array[id].selected = false;
+    // } else {
+    //   array[id].selected = true;
+    // }
+    array[id].selected = !array[id].selected;
+    // console.log('array of data', array);
+    // TODO put data to users
+    let interests = media.filter((obj) => {
+      return obj.selected;
+    });
+    interests = interests
+      .map((obj) => {
+        return obj.name;
+      })
+      .toString();
+    additionData.interests = interests;
+    user.full_name = JSON.stringify(additionData);
+    console.log('user data new', user);
+    delete user.user_id;
+    try {
+      await putUser(user, token);
+      console.log('modify ok');
+      setMedia(array);
+      setArrayState(arrayState);
+      // setLoading(!loading);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    arrayLoading();
+  }, [arrayState]);
+
   return (
     <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
-      <Text>Choose at least 3 interests</Text>
-      <View>
-        {/* <CheckBox
-          checkedIcon={''}
-          uncheckedIcon={''}
-          textStyle={yoga ? styles.true : styles.false}
-          containerStyle={yoga ? styles.boxTrue : styles.boxFalse}
-          title="Yoga"
-          checked={yoga}
-          onPress={() => setYoga(!yoga)}
-        ></CheckBox>
-        <CheckBox
-          checkedIcon={''}
-          uncheckedIcon={''}
-          textStyle={pets ? styles.true : styles.false}
-          containerStyle={pets ? styles.boxTrue : styles.boxFalse}
-          title="Pets"
-          checked={pets}
-          onPress={() => setPets(!pets)}
-        ></CheckBox>
-        <CheckBox
-          checkedIcon={''}
-          uncheckedIcon={''}
-          title="Food"
-          checked={food}
-          textStyle={food ? styles.true : styles.false}
-          containerStyle={food ? styles.boxTrue : styles.boxFalse}
-          onPress={() => setFood(!food)}
-        ></CheckBox> */}
-      </View>
-      <Button onPress={updateInterest}>Done</Button>
+      <Text style={{alignSelf: 'center', margin: 20}}>
+        Choose at least 3 interests
+      </Text>
+      <FlatList
+        contentContainerStyle={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+        }}
+        // numColumns={3}
+        showsHorizontalScrollIndicator={false}
+        data={media}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item}) => (
+          <Button
+            mode="outlined"
+            color={item.selected === true ? '#B6000B' : '#979797'}
+            style={
+              item.selected === true
+                ? {
+                    margin: 5,
+                    borderRadius: 50,
+                    borderWidth: 1,
+                    borderColor: '#B6000B',
+                  }
+                : {
+                    margin: 5,
+                    borderRadius: 50,
+                    borderWidth: 1,
+                    borderColor: '#979797',
+                  }
+            }
+            onPress={() => {
+              onPressHandler(item.id);
+            }}
+          >
+            {item.name}
+            {item.selected}
+          </Button>
+        )}
+      />
+      {/* <Button onPress={updateInterest}>Done</Button> */}
     </SafeAreaView>
   );
 };
