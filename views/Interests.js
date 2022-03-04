bc344eb44382da3e47924697c86e9471ea20ceb4
Amy Mai <My.Mai@metropolia.fi>
2022-03-04 09:32:55 +0200

    fix responsive issue

--- views/Interests.js
+++ views/Interests.js
@@ -1,54 +1,68 @@
-import {SafeAreaView, Text, View, StyleSheet, FlatList} from 'react-native';
+/* eslint-disable camelcase */
+import {SafeAreaView, Text, StyleSheet, FlatList} from 'react-native';
 import React, {useContext, useEffect, useState} from 'react';
-import {array, PropTypes} from 'prop-types';
+import {PropTypes} from 'prop-types';
 import GlobalStyles from '../utils/GlobalStyles';
 import {Button} from 'react-native-paper';
 import {dataSource} from '../utils/data';
 import {MainContext} from '../contexts/MainContext';
 import {useUser} from '../hooks/ApiHooks';
+import {
+  useFonts,
+  Poppins_600SemiBold,
+  Poppins_400Regular,
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Interests = ({navigation}) => {
+  const [fontsLoaded] = useFonts({
+    Poppins_600SemiBold,
+    Poppins_400Regular,
+  });
   const {putUser, getUserByToken} = useUser();
-  const {user, token} = useContext(MainContext);
+  const {user, token, loading, setLoading} = useContext(MainContext);
   let additionData = JSON.parse(user.full_name);
-  // let array = [];
   const [media, setMedia] = useState([]);
-  const [arrayState, setArrayState] = useState(
-    additionData.interests.split(',')
-  );
+  // const [arrayState, setArrayState] = useState(
+  //   additionData.interests.split(',')
+  // );
   let result = [];
 
   const arrayLoading = async () => {
+    // const token = await AsyncStorage.getItem('userToken');
     const user = await getUserByToken(token);
     additionData = JSON.parse(user.full_name);
-    const arrayInteres = additionData.interests.split(',');
+    let arrayInterest = additionData.interests.split(' ').join('').split(',');
+    arrayInterest = arrayInterest.map((obj) => obj.toLowerCase());
+    console.log('array interests', arrayInterest);
     let number = 0;
     // for (let i = 1; i < dataSource.length; i++) {
     result = dataSource.map((value) => {
       const obj = {
         id: number++,
         name: value,
-        selected: arrayInteres.includes(value) ? true : false,
+        selected: arrayInterest.includes(value.toLowerCase()) ? true : false,
       };
+      // console.log('check true false', arrayInterest.includes(value));
       return obj;
     });
-    setArrayState(result);
+    // setArrayState(arrayInterest);
     setMedia(result);
-    // console.log('datasouce', result);
+    console.log('datasouce', result);
   };
 
   const onPressHandler = async (id) => {
-    console.log('id', id);
+    // console.log('id', id);
     const array = media;
     array[id].selected = !array[id].selected;
     // console.log('array of data', array);
-    // TODO put data to users
     let interests = media.filter((obj) => {
       return obj.selected;
     });
     interests = interests
       .map((obj) => {
-        return obj.name;
+        return obj.name.toLowerCase();
       })
       .toString();
     additionData.interests = interests;
@@ -59,8 +73,8 @@ const Interests = ({navigation}) => {
       await putUser(user, token);
       console.log('modify ok');
       setMedia(array);
-      setArrayState(arrayState);
-      // setLoading(!loading);
+      // setArrayState(arrayState);
+      setLoading(!loading);
     } catch (error) {
       console.log(error);
     }
@@ -68,71 +82,88 @@ const Interests = ({navigation}) => {
 
   useEffect(() => {
     arrayLoading();
-  }, [arrayState]);
+  }, [loading]);
 
-  return (
-    <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
-      <Text style={{alignSelf: 'center', margin: 20}}>
-        Choose at least 3 interests
-      </Text>
-      <FlatList
-        contentContainerStyle={{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          flexWrap: 'wrap',
-        }}
-        // numColumns={3}
-        showsHorizontalScrollIndicator={false}
-        data={media}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({item}) => (
-          <Button
-            mode="outlined"
-            color={item.selected === true ? '#B6000B' : '#979797'}
-            style={
-              item.selected === true
-                ? {
-                    margin: 5,
-                    borderRadius: 50,
-                    borderWidth: 1,
-                    borderColor: '#B6000B',
-                  }
-                : {
-                    margin: 5,
-                    borderRadius: 50,
-                    borderWidth: 1,
-                    borderColor: '#979797',
-                  }
-            }
-            onPress={() => {
-              onPressHandler(item.id);
-            }}
-          >
-            {item.name}
-            {item.selected}
-          </Button>
-        )}
-      />
-      {/* <Button onPress={updateInterest}>Done</Button> */}
-    </SafeAreaView>
-  );
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
+        <FlatList
+          scrollEnabled={true}
+          scrollToOverflowEnabled={true}
+          ListHeaderComponent={
+            <Text style={styles.header}>Choose at least 3 interests</Text>
+          }
+          contentContainerStyle={{
+            flex: 1,
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+            marginTop: '5%',
+          }}
+          numColumns={3}
+          showsHorizontalScrollIndicator={false}
+          data={media}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => (
+            <Button
+              mode="outlined"
+              color={item.selected === true ? '#B6000B' : '#979797'}
+              style={
+                item.selected === true
+                  ? {
+                      margin: 5,
+                      borderRadius: 50,
+                      borderWidth: 1,
+                      borderColor: '#B6000B',
+                    }
+                  : {
+                      margin: 5,
+                      borderRadius: 50,
+                      borderWidth: 1,
+                      borderColor: '#979797',
+                    }
+              }
+              onPress={() => {
+                onPressHandler(item.id);
+              }}
+            >
+              {item.name}
+              {item.selected}
+            </Button>
+          )}
+          ListFooterComponent={
+            <Button
+              style={styles.button}
+              onPress={() => {
+                navigation.navigate('Profile');
+              }}
+            >
+              Done
+            </Button>
+          }
+        />
+      </SafeAreaView>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-  true: {
-    color: 'red',
-    marginLeft: 0,
+  header: {
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'Poppins_600SemiBold',
+    color: '#EB6833',
+    marginBottom: 10,
   },
-  false: {
-    color: 'black',
-  },
-  boxTrue: {
-    width: 100,
-    borderColor: 'red',
-  },
-  boxFalse: {
-    borderColor: 'black',
+  button: {
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#82008F',
+    width: '30%',
+    alignSelf: 'center',
+    borderRadius: 10,
   },
 });
 
