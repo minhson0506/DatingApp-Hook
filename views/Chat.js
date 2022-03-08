b1c42890cb4f200601832c0499c396b95d03de0a
Amy Mai <My.Mai@metropolia.fi>
2022-03-08 12:27:12 +0200

    fix minor bug

--- views/Chat.js
+++ views/Chat.js
@@ -45,6 +45,7 @@ const Chat = ({navigation}) => {
   const showMenu = () => setVisible(true);
 
   function isJson(str) {
+    if (str === null) return false;
     try {
       JSON.parse(str);
     } catch (e) {
